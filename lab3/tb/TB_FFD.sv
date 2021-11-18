6ae200929f51726e31eb30e3740954409f68522c
jquinonezb <89792961+jquinonezb@users.noreply.github.com>
2021-11-18 13:38:45 -0600

    New versions of testbench files

--- lab3/tb/TB_FFD.sv
+++ lab3/tb/TB_FFD.sv
@@ -1,13 +1,12 @@
 `timescale 1ps / 1ps
-module TB_FFD();
-parameter WIDTH = 4; 
-logic [WIDTH-1:0] D;
-logic clk, reset;
-logic [WIDTH-1:0] out;
-logic [WIDTH-1:0] gold;
-logic [WIDTH-1:0] error;
+module TB_FFD(); 
+logic  D;
+bit clk, reset;
+logic  q;
+logic  gold;
+//logic [WIDTH-1:0] error;
 
-FF_D #(WIDTH) Test (.Data_Input(D), .clk(clk), .Data_Output(out), .reset(reset));
+FF_D  Test (.d(D), .clk(clk), .q(q), .reset(reset));
 
 always
 begin 
@@ -17,33 +16,24 @@ end
 
 initial begin
 //Values 
-logic [WIDTH-1:0] gold;
+//logic [WIDTH-1:0] gold;
 reset <= 0;
-D <= 4'b0101;
+D <= 1'b1;
 clk <= 0;
-gold <= 4'b0;
+gold <= 1'b0;
 
 #15 reset <= 1; 
-#20 D <= 4'b0101; gold <= D;
+#20 D <= 1'b1; gold <= D;
 #15 reset <= 0; 
-#20 D <= 4'b0111;gold <= D;
+#20 D <= 1'b0;gold <= D;
 #15 reset <= 0; 
-#20 D <= 4'b0011; gold <= D;
+#20 D <= 1'b1; gold <= D;
+#15 reset <= 1; 
+#20 D <= 1'b1; gold <= D;
+#15 reset <= 1; 
+#20 D <= 1'b1; gold <= D;
+#15 reset <= 0; 
+#20 D <= 1'b0;gold <= D;
 end
-
-task comprobar();
-begin
-error = | (gold^out);
-`ifdef fatal
-	if (error) $fatal($fatal,"Esta es la directiva fatal");
-`elsif warning
-	if (error) $warning($time,"WARNING!");
-`elsif error
-	if (error) $error($time,"ERROR");
-`elsif info
-	if (error) $info($time,"Aquí hay información");
-`endif
-end
-endtask
 
 endmodule  
--- lab3/tb/run.do
+++ lab3/tb/run.do
@@ -1,7 +1,6 @@
 if [file exists work] {vdel -all}
 vlib work
-#vlog +define+pos_reset +define+info -f files.f
-vlog +define+ -f files.f
+vlog -f files.f
 onbreak {resume}
 set NoQuitOnFinish 1
 vsim -voptargs=+acc work.TB_FFD
