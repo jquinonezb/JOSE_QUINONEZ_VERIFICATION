d3b620609e4659e3f67d4c3a9fe7ab71c4b44faf
Leonardo Quiñonez <jose.quinonez@iteso.mx>
2022-01-05 00:12:47 -0600

    Run.do and Files.f

--- lab4/tb/files.f
+++ lab4/tb/files.f
@@ -1,5 +1,9 @@
-../src/FF_D_macro.sv
-../src/macro_FFD.def
-../src/macro_ST.def
-TB_FFD.sv
+../src/fifo_wrapper.sv
+../src/tb_fifo_pkg.sv
+../src/tb_if_aFIFO.sv
+../src/all_rtl_fifo.sv
+../src/fifo_if.sv
+../src/fifo_pkg.sv
+../src/tester_afifo.svh
+../src/tb_afifo.sv
 
--- lab4/tb/run.do
+++ lab4/tb/run.do
@@ -3,6 +3,6 @@ vlib work
 vlog -f files.f
 onbreak {resume}
 set NoQuitOnFinish 1
-vsim -voptargs=+acc work.TB_FFD
+vsim -voptargs=+acc work.tb_afifo.sv
 do wave.do
 run 1300ps
