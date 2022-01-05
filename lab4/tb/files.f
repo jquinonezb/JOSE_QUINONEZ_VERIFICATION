60cd2c12024edf68d0063cf10d986602f10f9ef7
Leonardo Quiñonez <jose.quinonez@iteso.mx>
2022-01-05 00:18:21 -0600

    Corrections to the files.f file

--- lab4/tb/files.f
+++ lab4/tb/files.f
@@ -1,9 +1,11 @@
-../src/fifo_wrapper.sv
-../src/tb_fifo_pkg.sv
-../src/tb_if_aFIFO.sv
-../src/all_rtl_fifo.sv
-../src/fifo_if.sv
 ../src/fifo_pkg.sv
-../src/tester_afifo.svh
+../src/tb_fifo_pkg.sv
+../src/fifo_wrapper.sv
 ../src/tb_afifo.sv
+../src/tb_if_aFIFO.sv
+../src/tester_afifo.svh
+../src/fifo_if.sv
+../src/all_rtl_fifo.sv
+
+
 
