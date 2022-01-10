2fed7afc0223edbf55776051d6f1582376905968
Leonardo Quiñonez <jose.quinonez@iteso.mx>
2022-01-09 18:54:23 -0600

    Changes to show behavior on wave

--- lab4/src/fifo_wrapper.sv
+++ lab4/src/fifo_wrapper.sv
@@ -1,9 +1,9 @@
 module fifo_wrapper
 import fifo_pkg::*;
 (
-    input  bit      wrclk,
+    input  bit      wr_clk,
     input  bit      wr_rst,
-    input  bit      rdclk,
+    input  bit      rd_clk,
     input  bit      rd_rst,
 
     input  data_t   data_in,
@@ -17,18 +17,18 @@ import fifo_pkg::*;
 fifo_if itf();
 always_comb begin
 	itf.data_in = data_in;
-	itf.push    = push;
+	itf.push    = {push};
 	full	    = itf.full;
 	data_out    = itf.data_out;
-	itf.pop     = pop;
+	itf.pop     = {pop};
 	empty	    = itf.empty;
 end
 
 fifo_top dut(
-	.clock_w(wrclk),
-	.reset_w(wr_rst),
-	.clock_r(rdclk),
-	.reset_r(rd_rst),
+	.wr_clk(wr_clk),
+	.wr_rst(wr_rst),
+	.rd_clk(rd_clk),
+	.rd_rst(rd_rst),
 	.itf(itf)
 );
 
--- lab4/tb/tb_afifo.sv
+++ lab4/tb/tb_afifo.sv
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+`timescale 100ps / 1ps
 `include "tester_afifo.svh";
 module tb_afifo();
 
@@ -8,85 +8,65 @@ bit 	clk_r,
 	clk_w, 
 	rst_w, 
 	rst_r;
-int  i;
+int  reset = 3;
+localparam 	times_push 	= 18,
+		times_pop 	= 16,
+		times_rep 	= 5;
 
 
+data_t empty, data_out;
+logic full;
 // Definition of tester
 tester_afifo  t;
 
 // Instance of interface
 tb_if_aFIFO   itf(
-	.clock_r(clk_r),
 	.clock_w(clk_w),
-	.reset_r(rst_r), 
-	.reset_w(rst_w)
+    	.reset_w(rst_w),
+    	.clock_r(clk_r),
+   	.reset_r(rst_r)
 );
 
 fifo_wrapper dut(
-    	.wrclk(clk_w),
+    	.wr_clk(clk_w),
     	.wr_rst(rst_w),
-    	.rdclk(clk_r),
-   	.rd_rst(rst_r),
+    	.rd_clk(clk_r),
+   	.rd_rst(rst_w),
 	.data_in(itf.data_in),
 	.push(itf.push),
     	.full(itf.full),
     	.data_out(itf.data_out),
     	.pop(itf.pop),
 	.empty(itf.empty)
+	
     );
 
 initial begin
-                t   = new(itf);
-                clk_r = 'd0; 
-                rst_r = 'd1;
-		clk_w = 'd0; 
-                rst_w = 'd1; 
-  #(2)   
-  		rst_r = 'd0;
-		rst_w = 'd0; 
-  #(2)   	rst_r = 'd1;
-		rst_w = 'd1; 
-
-        //Write
-        for (i = 0; i <= 10; i++) begin
-            #10 t.Push_Validation();
-        end
-      
-        //Read
-        for (i = 0; i <= 10; i++) begin
-            #10 t.Pop_validation(.counter(i));
-        end
-    end
-
-    //2nd case: Overflow
-    initial begin
-	//Write
-        for (i = 0; i <= 20; i++) begin
-            #10 t.Push_Validation();
-        end
-        
-        //Read
-          for (i = 0; i <= 20; i++) begin
-            #10 t.Pop_validation(.counter(i));
-        end
-    end
-
-    //3rd case: Underflow
-    initial begin
-        for (i = 0; i <= 15; i++) begin
-            #10 t.Push_Validation();
-        end
-
-        //Read
-         for (i = 0; i <= 20; i++) begin
-            #10 t.Pop_validation(.counter(i));
-        end
-	  $stop;
-    end
-
-    initial begin
+fork
     	forever #1 clk_r = !clk_r;
-	forever #1 clk_w = !clk_w;
-    end
-  
+	forever #2 clk_w = !clk_w;
+join
+end
+
+initial begin
+                t   = new(itf);
+        
+	repeat(reset) begin 		
+		#3 	rst_w = ~rst_w;
+			rst_r = ~rst_r;
+	end
+
+	repeat(times_rep)begin 
+	//Random_flags();
+	
+	//PUSH
+	#2 repeat(times_push)begin
+	t.inj_one_data(.full(full));
+	end
+        //POP
+        #2 repeat(times_pop)begin
+	t.ext_one_data(.data_out(data_out), .empty(empty));
+	end 
+	end
+end 
 endmodule
